482ef77b5148a70c5cea6dcb0409f28a8a3ab590
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2026-02-07 17:43:41 +0100

    feat: Add Prometheus Integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	// implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 	implementation("org.flywaydb:flyway-database-postgresql")
